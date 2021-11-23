3a849493f6ef82b0f4c542f47ec36c7959abd888
dohoseok <44964776+dohoseok@users.noreply.github.com>
2021-11-23 19:49:38 +0900

    Update cifar10_example_1.py

--- cifar10_example_1.py
+++ cifar10_example_1.py
@@ -33,24 +33,24 @@ device = 'cuda' if cuda.is_available() else 'cpu'
 print("Train Dataset Size", len(train_loader.dataset))
 print("Test Dataset Size", len(test_loader.dataset))
 
-for batch_idx, (data, target) in enumerate(train_loader):
-  print(data.shape)
-  print(target.shape)
-  break
+# for batch_idx, (data, target) in enumerate(train_loader):
+#   print(data.shape)
+#   print(target.shape)
+#   break
 
 ######################################################
 
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+# def imshow(img):
+#     img = img / 2 + 0.5     # unnormalize
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#     plt.show()
 
-# get some random training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
+# # get some random training images
+# dataiter = iter(train_loader)
+# images, labels = dataiter.next()
 
-# show images
-imshow(torchvision.utils.make_grid(images[:4]))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# # show images
+# imshow(torchvision.utils.make_grid(images[:4]))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
